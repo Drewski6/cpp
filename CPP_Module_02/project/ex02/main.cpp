6ae67087bf01a8b939166177bdda97753ce6f0ba
Drew Pentland <drewtpentland@gmail.com>
2024-01-27 13:20:43 +0100

    m02/ex01 wasnt finished. Now complete
    I had used a forbidden function (std::pow) and need to refactor
    my code to fit with the stipulations of the subject.

---
+++ CPP_Module_02/project/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 18:31:00 by dpentlan          #+#    #+#             */
+/*   Updated: 2024/01/27 12:32:17 by dpentlan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.h"
+#include <iostream>
+
+int main(void) {
+
+  Fixed a;
+  Fixed const b(Fixed(5.05f) * Fixed(2));
+
+  std::cout << a << std::endl;
+  std::cout << ++a << std::endl;
+  std::cout << a << std::endl;
+  std::cout << a++ << std::endl;
+  std::cout << a << std::endl;
+
+  std::cout << b << std::endl;
+
+  std::cout << Fixed::max(a, b) << std::endl;
+
+  return 0;
+}
