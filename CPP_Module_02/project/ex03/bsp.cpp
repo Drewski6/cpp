c2bddd16930351bb8583e049d3f91762b58d0b06
Drew Pentland <drewtpentland@gmail.com>
2024-01-31 21:52:03 +0100

    m02/ex03 finished module 2
    finished bsp exercise and added 10 tests.

--- CPP_Module_02/project/ex03/bsp.cpp
+++ CPP_Module_02/project/ex03/bsp.cpp
@@ -6,16 +6,46 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 19:54:40 by dpentlan          #+#    #+#             */
-/*   Updated: 2024/01/31 20:29:16 by dpentlan         ###   ########.fr       */
+/*   Updated: 2024/01/31 21:42:16 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.h"
 
+float areaTriangle(Point const a, Point const b, Point const c) {
+  // Formula: 0.5 * abs( x0(y1 - y2) + x1(y2 - y0) + x2(y0 - y1) )
+  float ret =
+      (a.getX().toFloat() * (b.getY().toFloat() - c.getY().toFloat()) +
+       (b.getX().toFloat() * (c.getY().toFloat() - a.getY().toFloat())) +
+       (c.getX().toFloat() * (a.getY().toFloat() - b.getY().toFloat()))) *
+      0.5;
+  if (ret < 0)
+    ret *= -1;
+  return ret;
+}
+
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
-  (void)a;
-  (void)b;
-  (void)c;
-  (void)point;
-  return true;
+  float areaT1 = areaTriangle(point, b, c);
+  float areaT2 = areaTriangle(a, point, c);
+  float areaT3 = areaTriangle(a, b, point);
+  float areaTOrig = areaTriangle(a, b, c);
+
+  // Print statements for debugging if you want...
+  // std::cout << "areaT1: " << areaT1 << std::endl;
+  // std::cout << "areaT2: " << areaT2 << std::endl;
+  // std::cout << "areaT3: " << areaT3 << std::endl;
+  // std::cout << "areaTOrig: " << areaTOrig << std::endl;
+
+  // If area of any are 0 that means point is on the edge or vertex.
+  // false condition
+  if (!areaT1 || !areaT2 || !areaT3)
+    return (false);
+
+  // If sum of 3 triangles is greater than original, then point is outside.
+  // false
+  if ((areaT1 + areaT2 + areaT3) > areaTOrig)
+    return (false);
+
+  // else, true
+  return (true);
 }
--- CPP_Module_02/project/ex03/main.cpp
+++ CPP_Module_02/project/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 18:31:00 by dpentlan          #+#    #+#             */
-/*   Updated: 2024/01/31 20:36:46 by dpentlan         ###   ########.fr       */
+/*   Updated: 2024/01/31 21:49:57 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ bool bsp(Point const a, Point const b, Point const c, Point const point);
 int main(void) {
   Point p1(0, 0);
   Point p2(5, 0);
-  Point p3(0, 4);
-  Point point(2, 2);
+  Point p3(0, 5);
+  Point point(0, 4);
 
   // Output to guide user.
   std::cout << "Given a triangle with verticies: " << std::endl;
@@ -35,9 +35,106 @@ int main(void) {
             << std::endl;
 
   // Additional tests as requested in the subject.
+  // Tests on triangle:
+  // 5 | *
+  // 4 | |\
+  // 3 | | \
+  // 2 | |  \
+  // 1 | |   \
+  // 0 | *----*
+  //     012345
   {
     std::cout << "Tests:" << std::endl;
-    // insert tests here
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(0, 4);
+      std::cout << "Test 1 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(4, 0);
+      std::cout << "Test 2 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(0, 0);
+      std::cout << "Test 3 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(1, 1);
+      std::cout << "Test 4 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(1, 4);
+      std::cout << "Test 5 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(1.1, 4);
+      std::cout << "Test 6 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(0.9, 4);
+      std::cout << "Test 7 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(5, 5);
+      std::cout << "Test 8 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(-1, -1);
+      std::cout << "Test 9 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
+
+    {
+      Point p1(0, 0);
+      Point p2(5, 0);
+      Point p3(0, 5);
+      Point point(5, -4);
+      std::cout << "Test 10 Result: "
+                << (bsp(p1, p2, p3, point) ? "true" : "false") << std::endl;
+    }
   }
   return (0);
 }
