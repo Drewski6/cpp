7c853c3eb59a9c81a16a09db0fd2036099d06f14
Drew <91377105+Drewski6@users.noreply.github.com>
2024-02-13 15:45:55 +0100

    Adding some more tests to m01/ex03

--- CPP_Module_01/project/ex03/main.cpp
+++ CPP_Module_01/project/ex03/main.cpp
@@ -6,29 +6,47 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 23:07:07 by dpentlan          #+#    #+#             */
-/*   Updated: 2024/01/17 14:28:29 by dpentlan         ###   ########.fr       */
+/*   Updated: 2024/02/13 15:45:19 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.h"
 #include "HumanB.h"
 #include "Weapon.h"
+#include <iostream>
 
 int main() {
   {
-    Weapon club = Weapon("crude spiked club");
-    HumanA bob("Bob", club);
-    bob.attack();
-    club.setType("some other type of club");
-    bob.attack();
+    std::cout << "===== Test 1 =====" << std::endl;
+    // Example from subject
+    {
+      Weapon club = Weapon("crude spiked club");
+      HumanA bob("Bob", club);
+      bob.attack();
+      club.setType("some other type of club");
+      bob.attack();
+    }
+    {
+      Weapon club = Weapon("crude spiked club");
+      HumanB jim("Jim");
+      jim.setWeapon(club);
+      jim.attack();
+      club.setType("some other type of club");
+      jim.attack();
+    }
   }
+
   {
-    Weapon club = Weapon("crude spiked club");
-    HumanB jim("Jim");
-    jim.setWeapon(club);
-    jim.attack();
-    club.setType("some other type of club");
-    jim.attack();
+    std::cout << "\n===== Test 2 =====" << std::endl;
+    // Example using pointers
+    Weapon *bb = new Weapon("Baseball bat");
+    if (bb == NULL)
+      return (1);
+    HumanA billy("Billy", *bb);
+    billy.attack();
+
+    delete bb;
   }
+
   return 0;
 }
