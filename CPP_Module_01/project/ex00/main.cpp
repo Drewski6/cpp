34917877e824b6f4900c759d000706632f0d2ed2
Drew <91377105+Drewski6@users.noreply.github.com>
2024-02-13 13:27:12 +0100

    m01/ex00 - added some tests

--- CPP_Module_01/project/ex00/main.cpp
+++ CPP_Module_01/project/ex00/main.cpp
@@ -6,17 +6,42 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 14:49:36 by dpentlan          #+#    #+#             */
-/*   Updated: 2024/01/14 18:56:46 by dpentlan         ###   ########.fr       */
+/*   Updated: 2024/02/13 13:27:02 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.h"
+#include <iostream>
 
 int main() {
-  Zombie *my_zombie = newZombie("Joe");
-  my_zombie->announce();
+  std::cout << "\n===== Test 1 =====" << std::endl;
 
-  randomChump("Mike");
-  delete my_zombie;
+  {
+    // Test of allocation on the heap and using associated functions.
+    Zombie *my_zombie = newZombie("Joe");
+    my_zombie->announce();
+
+    randomChump("Mike");
+    delete my_zombie;
+  }
+
+  std::cout << "\n===== Test 2 =====" << std::endl;
+
+  {
+    // Test of allocation on the heap
+    Zombie *a_zombie = new Zombie("Bob");
+    a_zombie->announce();
+
+    delete a_zombie;
+  }
+
+  std::cout << "\n===== Test 3 =====" << std::endl;
+
+  {
+    // Test of creation on the stack
+    Zombie new_zombie = Zombie("My_Name_Is_Local_Scope");
+    new_zombie.announce();
+  }
+
   return (0);
 }
