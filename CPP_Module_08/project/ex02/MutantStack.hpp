cae42ecbc871a3290dde1ae65b9f8638a40c1628
Drew Pentland <drewtpentland@gmail.com>
2024-03-29 18:44:01 -0500

    m08/ex02 - Starting project

---
+++ CPP_Module_08/project/ex02/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 15:12:14 by dpentlan          #+#    #+#             */
+/*   Updated: 2024/03/29 18:43:50 by dpentlan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# pragma once
+
+#include <stack>
+
+template <typename T>
+class MutantStack {
+private:
+  std::stack<T> _stack;
+public:
+  // done for today
+};
+
+#include "MutantStack.tpp"
