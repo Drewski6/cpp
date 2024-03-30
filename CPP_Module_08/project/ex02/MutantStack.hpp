9c886fd53a1e74d2ad3572d80f899c4216ce90bc
Drew Pentland <drewtpentland@gmail.com>
2024-03-30 11:02:06 -0500

    m08/ex02 - WIP
    Finished creating base MutantStack that acts the same as std::stack.

--- CPP_Module_08/project/ex02/MutantStack.hpp
+++ CPP_Module_08/project/ex02/MutantStack.hpp
@@ -6,20 +6,36 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 15:12:14 by dpentlan          #+#    #+#             */
-/*   Updated: 2024/03/29 18:43:50 by dpentlan         ###   ########.fr       */
+/*   Updated: 2024/03/30 10:36:23 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # pragma once
 
 #include <stack>
+#include <cstddef>
 
 template <typename T>
 class MutantStack {
-private:
-  std::stack<T> _stack;
 public:
-  // done for today
+  // Constructors , Copy Constructor, Destructor
+  MutantStack(void);
+  MutantStack(MutantStack const &source);
+  ~MutantStack(void);
+
+  // Overloaded Operators
+  MutantStack &operator=(MutantStack const &rhs);
+
+  // Public Methods
+  T &top();
+  bool empty();
+  size_t size();
+  void push(const T &value);
+  void pop();
+  
+private:
+  // Only deque available as underlying container for my MutatedStack.
+  std::stack<T> _stack;
 };
 
 #include "MutantStack.tpp"
--- CPP_Module_08/project/ex02/main.cpp
+++ CPP_Module_08/project/ex02/main.cpp
@@ -6,13 +6,171 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 09:06:08 by dpentlan          #+#    #+#             */
-/*   Updated: 2024/02/01 12:47:31 by dpentlan         ###   ########.fr       */
+/*   Updated: 2024/03/30 11:01:29 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
 #include <iostream>
+#include <list>
+#include <stack>
 
 int main() {
   // main
+  {
+    std::cout << "\n===== Test 1 : Basic Constructors =====\n" << std::endl;
+    // Tests default constructor, copy constructor and copy assignment.
+
+    MutantStack<int> mstack;  // default constructor
+    MutantStack<int> ustack;  // default constructor
+
+    std::cout << "Adding 190 to mstack then copy to ustack\n" << std::endl;
+    mstack.push(190);
+    ustack = mstack;          // copy assignment operator
+
+    std::cout << "mstack size       : " << mstack.size() << std::endl;
+    std::cout << "mstack top        : " << mstack.top() << std::endl;
+    std::cout << "mstack._stack addr: " << &(mstack.top()) << std::endl;
+
+    std::cout << "ustack size       : " << ustack.size() << std::endl;
+    std::cout << "ustack top        : " << ustack.top() << std::endl;
+    std::cout << "ustack._stack addr: " << &(ustack.top()) << std::endl;
+
+    std::cout << "\nAdding 270 to mstack but not ustack\n" << std::endl;
+    mstack.push(270);
+
+    std::cout << "mstack size       : " << mstack.size() << std::endl;
+    std::cout << "mstack top        : " << mstack.top() << std::endl;
+    std::cout << "mstack._stack addr: " << &(mstack.top()) << std::endl;
+
+    std::cout << "ustack size       : " << ustack.size() << std::endl;
+    std::cout << "ustack top        : " << ustack.top() << std::endl;
+    std::cout << "ustack._stack addr: " << &(ustack.top()) << std::endl;
+
+    std::cout << "\nCreating ystack with copy constructor\n" << std::endl;
+    MutantStack<int> ystack = mstack;
+
+    std::cout << "ystack size       : " << ystack.size() << std::endl;
+    std::cout << "ystack top        : " << ystack.top() << std::endl;
+    std::cout << "ystack._stack addr: " << &(ystack.top()) << std::endl;
+    
+  }
+
+  {
+    std::cout << "\n===== Test 2 : Stack Methods =====\n" << std::endl;
+    // Stack Methods //
+
+    MutantStack<int> mstack;
+
+    std::cout << "Is mstack empty? " << (mstack.empty() ? "true" : "false")
+              << std::endl;
+
+    std::cout << "\nPushing ints to stack\n" << std::endl;
+    mstack.push(111);
+    mstack.push(222);
+    mstack.push(333);
+    mstack.push(444);
+    mstack.push(555);
+
+    std::cout << "Is mstack empty? " << (mstack.empty() ? "true" : "false")
+              << std::endl;
+
+    std::cout << "mstack size: " << mstack.size() << std::endl;
+    std::cout << "mstack top : " << mstack.top() << std::endl;
+
+    std::cout << "\nPopping stack\n" << std::endl;
+    mstack.pop();
+
+    std::cout << "mstack size: " << mstack.size() << std::endl;
+    std::cout << "mstack top : " << mstack.top() << std::endl;
+
+  }
+
+  // {
+  //   std::cout << "\n===== Test 3 : Test from Subject =====\n" << std::endl;
+  //   // Test from Subject //
+  //
+  //   MutantStack<int> mstack;
+  //
+  //   mstack.push(5);
+  //   mstack.push(17);
+  //
+  //   std::cout << mstack.top() << std::endl;
+  //
+  //   mstack.pop();
+  //
+  //   std::cout << mstack.size() << std::endl;
+  //
+  //   mstack.push(3);
+  //   mstack.push(5);
+  //   mstack.push(737);
+  //   //[...]
+  //   mstack.push(0);
+  //
+  //   MutantStack<int>::iterator it = mstack.begin();
+  //   MutantStack<int>::iterator ite = mstack.end();
+  //
+  //   ++it;
+  //   --it;
+  //   while (it != ite)
+  //   {
+  //     std::cout << *it << std::endl;
+  //     ++it;
+  //   }
+  //   std::stack<int> s(mstack);
+  //   // return 0; // Not returning yet.
+  // }
+
+  {
+    std::cout << "\n===== Test 4 : Test with std::list (subject) =====\n"
+              << std::endl;
+    // Test with std::list (subject) //
+
+    std::list<int> myList;
+
+    myList.push_back(5);
+    myList.push_back(17);
+
+    std::cout << myList.back() << std::endl;
+
+    myList.pop_back();
+
+    std::cout << myList.size() << std::endl;
+
+    myList.push_back(3);
+    myList.push_back(5);
+    myList.push_back(737);
+    //[...]
+    myList.push_back(0);
+
+    std::list<int>::iterator it = myList.begin();
+    std::list<int>::iterator ite = myList.end();
+
+    ++it;
+    --it;
+    while (it != ite)
+    {
+      std::cout << *it << std::endl;
+      ++it;
+    }
+    std::stack<int, std::list<int> > s(myList);
+    // return 0; // Not returning yet.
+  }
+  
+  {
+    std::cout << "\n===== Test 5 : Doubles =====\n" << std::endl;
+    // Doubles //
+
+    
+  }
+  
+  {
+    std::cout << "\n===== Test 6 : Strings =====\n" << std::endl;
+    // Strings //
+
+    
+  }
+  
+  
   return (0);
 }
