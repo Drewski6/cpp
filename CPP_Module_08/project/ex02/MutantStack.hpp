afad4e516beaefe4ad39b20229465a61cd0631a7
Drew Pentland <drewtpentland@gmail.com>
2024-04-01 11:01:19 -0500

    m08/ex02 - Almost done
    Restarted and rather than wrapping all the stack functions, I'm
    inheriting the stack class directly.
    To return iterators I'm now just accessing the stack's underlying
    container class' iterators directly rather than creating my own.
    Always better to use what's already there.

--- CPP_Module_08/project/ex02/MutantStack.hpp
+++ CPP_Module_08/project/ex02/MutantStack.hpp
@@ -6,36 +6,31 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 15:12:14 by dpentlan          #+#    #+#             */
-/*   Updated: 2024/03/30 10:36:23 by dpentlan         ###   ########.fr       */
+/*   Updated: 2024/04/01 11:00:49 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# pragma once
+#pragma once
 
 #include <stack>
-#include <cstddef>
 
 template <typename T>
-class MutantStack {
+class MutantStack : public std::stack<T> {
 public:
-  // Constructors , Copy Constructor, Destructor
-  MutantStack(void);
-  MutantStack(MutantStack const &source);
-  ~MutantStack(void);
+  // Creating new type with name "iterator" which is a "typename std::stack<T>::container_type::iterator"
+  // This type acts as an iterator for whichever type of iterator we want to return for the underlying container.
+  typedef typename std::stack<T>::container_type::iterator iterator;
 
-  // Overloaded Operators
-  MutantStack &operator=(MutantStack const &rhs);
+  // Returning our new type "iterator" by calling begin, end, etc for the underlying container by accessing through c.
+  // c is the underlying container object in the stack class. So we're calling its begin method rather than making our own.
+  iterator begin() { return std::stack<T>::c.begin(); }
+  iterator end() { return std::stack<T>::c.end(); }
+  iterator rbegin() { return std::stack<T>::c.rbegin(); }
+  iterator rend() { return std::stack<T>::c.rend(); }
 
-  // Public Methods
-  T &top();
-  bool empty();
-  size_t size();
-  void push(const T &value);
-  void pop();
-  
-private:
-  // Only deque available as underlying container for my MutatedStack.
-  std::stack<T> _stack;
+  // Same for const versions of our iterators.
+  iterator cbegin() { return std::stack<T>::c.cbegin(); }
+  iterator cend() { return std::stack<T>::c.cend(); }
+  iterator crbegin() { return std::stack<T>::c.crbegin(); }
+  iterator crend() { return std::stack<T>::c.crend(); }
 };
-
-#include "MutantStack.tpp"
--- CPP_Module_08/project/ex02/main.cpp
+++ CPP_Module_08/project/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 09:06:08 by dpentlan          #+#    #+#             */
-/*   Updated: 2024/03/30 11:01:29 by dpentlan         ###   ########.fr       */
+/*   Updated: 2024/04/01 10:42:32 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ int main() {
     std::cout << "\n===== Test 1 : Basic Constructors =====\n" << std::endl;
     // Tests default constructor, copy constructor and copy assignment.
 
-    MutantStack<int> mstack;  // default constructor
-    MutantStack<int> ustack;  // default constructor
+    MutantStack<int> mstack; // default constructor
+    MutantStack<int> ustack; // default constructor
 
     std::cout << "Adding 190 to mstack then copy to ustack\n" << std::endl;
     mstack.push(190);
-    ustack = mstack;          // copy assignment operator
+    ustack = mstack; // copy assignment operator
 
     std::cout << "mstack size       : " << mstack.size() << std::endl;
     std::cout << "mstack top        : " << mstack.top() << std::endl;
@@ -53,7 +53,6 @@ int main() {
     std::cout << "ystack size       : " << ystack.size() << std::endl;
     std::cout << "ystack top        : " << ystack.top() << std::endl;
     std::cout << "ystack._stack addr: " << &(ystack.top()) << std::endl;
-    
   }
 
   {
@@ -83,43 +82,41 @@ int main() {
 
     std::cout << "mstack size: " << mstack.size() << std::endl;
     std::cout << "mstack top : " << mstack.top() << std::endl;
-
   }
 
-  // {
-  //   std::cout << "\n===== Test 3 : Test from Subject =====\n" << std::endl;
-  //   // Test from Subject //
-  //
-  //   MutantStack<int> mstack;
-  //
-  //   mstack.push(5);
-  //   mstack.push(17);
-  //
-  //   std::cout << mstack.top() << std::endl;
-  //
-  //   mstack.pop();
-  //
-  //   std::cout << mstack.size() << std::endl;
-  //
-  //   mstack.push(3);
-  //   mstack.push(5);
-  //   mstack.push(737);
-  //   //[...]
-  //   mstack.push(0);
-  //
-  //   MutantStack<int>::iterator it = mstack.begin();
-  //   MutantStack<int>::iterator ite = mstack.end();
-  //
-  //   ++it;
-  //   --it;
-  //   while (it != ite)
-  //   {
-  //     std::cout << *it << std::endl;
-  //     ++it;
-  //   }
-  //   std::stack<int> s(mstack);
-  //   // return 0; // Not returning yet.
-  // }
+  {
+    std::cout << "\n===== Test 3 : Test from Subject =====\n" << std::endl;
+    // Test from Subject //
+
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+    while (it != ite) {
+      std::cout << *it << std::endl;
+      ++it;
+    }
+    std::stack<int> s(mstack);
+    // return 0; // Not returning yet.
+  }
 
   {
     std::cout << "\n===== Test 4 : Test with std::list (subject) =====\n"
@@ -148,29 +145,31 @@ int main() {
 
     ++it;
     --it;
-    while (it != ite)
-    {
+    while (it != ite) {
       std::cout << *it << std::endl;
       ++it;
     }
-    std::stack<int, std::list<int> > s(myList);
+    std::stack<int, std::list<int> > s(myList); // need to use std::list stack
     // return 0; // Not returning yet.
   }
-  
+
   {
     std::cout << "\n===== Test 5 : Doubles =====\n" << std::endl;
     // Doubles //
-
-    
   }
-  
+
   {
     std::cout << "\n===== Test 6 : Strings =====\n" << std::endl;
     // Strings //
-
-    
   }
-  
-  
+
+  {
+    std::cout << "\n===== Test 7 : Algorithms =====\n" << std::endl;
+    // What's the point of iterators if they dont work with algorithms //
+
+    // test with std::count, std::max_element, std::min_element,
+    // and std::transform to create a new stack
+  }
+
   return (0);
 }
