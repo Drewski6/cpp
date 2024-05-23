7db7954ada2feb713457ef8c9dc2e48909418838
Drew <91377105+Drewski6@users.noreply.github.com>
2024-05-23 09:04:52 +0200

    m09 fixed some comments

--- CPP_Module_09/project/ex00/BitcoinExchange.cpp
+++ CPP_Module_09/project/ex00/BitcoinExchange.cpp
@@ -32,17 +32,17 @@ BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &rhs) {
 
 void BitcoinExchange::dBImport() {
   std::ifstream dBFile(_dBFileName.c_str());
-
+  // check if file is open
   if (dBFile.is_open() != true) {
     throw DBImportException();
   }
-
+  // init variables
   std::string line;
   std::string date;
   std::string value_str;
   double value;
   char *end = 0;
-
+  // Read file and insert into map
   while (std::getline(dBFile, line)) {
     date = line.substr(0, 10);
     value_str = line.substr(11);
@@ -51,17 +51,17 @@ void BitcoinExchange::dBImport() {
       throw ValueTooLargeException();
     _map.insert(std::make_pair(date, value));
   }
-
+  // Remove the header
   if (_map.find("date,excha") != _map.end())
     _map.erase("date,excha");
-
+  // Close file
   dBFile.close();
 }
 
 std::string BitcoinExchange::evaluateOutput(std::string &inputStr) {
   std::stringstream ss;
   double value = _inputParse(inputStr);
-
+  // format output string
   ss << inputStr.substr(0, 10) << " => " << value << " = "
      << _valueLookup(inputStr.substr(0, 10)) * value;
   return (ss.str());
@@ -83,7 +83,6 @@ double BitcoinExchange::_valueLookup(std::string lookup) {
   return it->second;
 };
 
-// NOT FINISHED
 double BitcoinExchange::_inputParse(std::string inputStr) {
   std::stringstream ss;
   std::string date_str;
@@ -127,27 +126,28 @@ bool BitcoinExchange::_isValidDate(std::string dateStr) {
   // Check length of the string
   if (dateStr.length() != 10)
     return false;
-  // Check format
+  // Check dashes are used to separate year, month, and day
   if (dateStr[4] != '-' || dateStr[7] != '-')
     return false;
-  // Check if year, month, and day are numerical
+  // Check that only digits are used for year, month, and day
   for (int i = 0; i < 10; ++i) {
     if (i == 4 || i == 7)
       continue;
     if (!isdigit(dateStr[i]))
       return false;
   }
-  // Extract year, month, and day
+  // use istringstream to extract year, month, and day
   std::istringstream iss(dateStr);
   int year, month, day;
   char dash;
   iss >> year >> dash >> month >> dash >> day;
-  // Check range of year, month, and day
+  // Check ranges of year, month, and day
   if (year < 0 || month < 1 || month > 12 || day < 1 || day > 31)
     return false;
-  // Check for valid number of days in month
+  // Check april, june, september, november have 30 days
   if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
     return false;
+  // check February has 28 or 29 days
   if (month == 2) {
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
       if (day > 29)
